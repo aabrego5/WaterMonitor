6ab68aa341d68b7f3188e6ab1595357c59458677
vinishavenu53 <vinishavenu@utexas.edu>
2019-10-28 18:39:25 -0500

    HomePage - PieChart

--- app/src/main/java/com/example/watermonitor/HomePage.java
+++ app/src/main/java/com/example/watermonitor/HomePage.java
@@ -21,17 +21,13 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         pieChartView = findViewById(R.id.chart);
-        pieData.add(new SliceValue(15, Color.BLUE));
-        pieData.add(new SliceValue(25, Color.GRAY));
-        pieData.add(new SliceValue(10, Color.RED));
-        pieData.add(new SliceValue(60, Color.MAGENTA));
-        pieChartView.setPieChartData(pieChartData);
-        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
-        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
-        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
-        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Bathroom Sink"));
+        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Kitchen Sink"));
+        pieData.add(new SliceValue(10, Color.RED).setLabel("Shower"));
+        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Toilet"));
         pieChartData.setHasLabels(true);
         pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+        pieChartView.setPieChartData(pieChartData);
 
     }
 }
