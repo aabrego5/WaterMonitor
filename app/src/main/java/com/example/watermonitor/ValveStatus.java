74619e61f1fd997f3b7ef07a8239b394c756c79d
John Anderson <johnleon25@yahoo.com>
2020-03-04 14:07:06 -0600

    Merge branch 'master' of /Users/johnanderson/SeniorDesign/WaterMonitor with conflicts.

--- app/src/main/java/com/example/watermonitor/ValveStatus.java
+++ app/src/main/java/com/example/watermonitor/ValveStatus.java
@@ -15,9 +15,9 @@ public class ValveStatus extends RealmObject{
     public ValveStatus(String app, int pcnt) {
         appliance = app;
         isChanged = false;
-        if(pcnt > 100) valveOpenPcnt = 100;
-        else if(pcnt < 0) valveOpenPcnt = 0;
-        else valveOpenPcnt = pcnt;
+        if(pcnt > 100) valveOpenPcnt = newValveOpenPcnt = 100;
+        else if(pcnt < 0) valveOpenPcnt = newValveOpenPcnt = 0;
+        else valveOpenPcnt = newValveOpenPcnt = pcnt;
     }
 
     public ValveStatus(String app) {
@@ -35,7 +35,7 @@ public class ValveStatus extends RealmObject{
         if(pcnt < 0) pcnt = 0;
 
         if(pcnt != valveOpenPcnt) {
-            newValveOpenPcnt = pcnt;
+            valveOpenPcnt = pcnt;
             isChanged = true;
         }
     }
